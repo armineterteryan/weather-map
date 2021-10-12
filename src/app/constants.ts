70bea17fee0762261b0e0d1a64f8c108b092ed1a
Armineterteryan <armine@identio.fi>
2021-10-12 09:44:04 +0300

    constants.ts added

---
+++ src/app/constants.ts
@@ -0,0 +1,5 @@
+export class Constants {
+  public static baseUrl: string = 'http://api.openweathermap.org/data/2.5/weather';
+  public static appid: string = '81a55167e2500308e5ec3496620e76e8';
+  public static units: string = 'metric';
+}
--- src/app/services/weather-map.service.ts
+++ src/app/services/weather-map.service.ts
@@ -1,21 +1,21 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
-
-const baseUrl = 'http://api.openweathermap.org/data/2.5/weather';
-const appid = '81a55167e2500308e5ec3496620e76e8';
-const units = 'metric';
+import {Constants} from "../constants";
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherMapService {
 
+  baseUrl: string = Constants.baseUrl;
+  appid: string = Constants.appid;
+  units: string = Constants.units;
+
   constructor(private http: HttpClient) {
   }
 
   get(name: string): Observable<any> {
-    return this.http.get(`${baseUrl}/?q=${name}&appid=${appid}&units=${units}`);
+    return this.http.get(`${this.baseUrl}/?q=${name}&appid=${this.appid}&units=${this.units}`);
   }
-
 }
